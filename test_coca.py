86094e79aa2b20dea1fe7ea3f0190643cf47f54b
songyuanwei <song.yuanwei@huawei.com>
2024-03-08 16:36:48 +0800

    coca

---
+++ test_coca.py
@@ -0,0 +1,47 @@
+import yaml
+from PIL import Image
+from coca import CoCa
+from modules.encoders.tokenizer import BpeTokenizer
+import mindspore as ms
+from mindspore import Tensor, ops
+from mindspore import dataset as ds
+from mindspore.dataset import vision
+
+def create_model(config):
+    
+    with open(config, "r", ) as f:
+        model_cfg = yaml.load(f, Loader=yaml.FullLoader)
+    # print(model_cfg)
+    model = CoCa(**model_cfg)
+
+    return model
+
+
+
+
+
+
+if __name__ == "__main__":
+    config = "/disk1/mindone/songyuanwei/mindone/examples/coca/coca_ViT-L-14.yaml"
+    ms.set_context(mode=ms.PYNATIVE_MODE)
+    
+    im = Image.open("/disk1/mindone/songyuanwei/mindone/examples/coca/cat.jpg").convert("RGB")
+    im = vision.ToTensor()(im)
+    im = ms.Tensor(im).expand_dims(0)
+    im = ops.ResizeBilinearV2()(im, (224, 224))
+    # im = im.squeeze()
+    print(im.shape)
+    model = create_model(config)
+    # with open("coca_ms_weight.text", "w", ) as f:
+    #     for param in model.get_parameters():
+    #         name = param.name
+    #         value = param.data.asnumpy()
+    #         f.write(f"{name}: {value.shape} \n")
+    #         # print(name, value.shape)
+
+    model.set_train(False)
+    text = model.generate(im)
+    tokenizer = BpeTokenizer()
+    # output = tokenizer.decode(text[0].asnumpy()).split("<endoftext>")[0].replace("<startoftext>", "")
+    output = tokenizer.decode(text[0].asnumpy())
+    print(output)
